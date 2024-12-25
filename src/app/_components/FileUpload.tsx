64ab381583b57c5c7a4e2fa77cba5bafd9850599
Dylan James <alazyartist@gmail.com>
2024-12-24 21:28:14 -0700

    Upload Button

---
+++ src/app/_components/FileUpload.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { FileUpload } from "@/components/ui/file-upload";
+
+const UploadForm = () => {
+  return (
+    <div className="h-full w-full rounded-xl">
+      <FileUpload />
+    </div>
+  );
+};
+
+export default UploadForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
+import UploadForm from "./_components/FileUpload";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -10,14 +11,13 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="dark flex min-h-screen flex-col items-center justify-center text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <main className="dark flex min-h-screen w-[calc(100%-8px)] flex-col items-center justify-start text-white">
+        <div className="container flex h-full flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Sprite <span className="text-[hsl(176,64%,55%)]">Sheet</span> ME
           </h1>
-          <div className="flex flex-col items-center gap-2 rounded-xl bg-zinc-800 p-12">
-            <p className="text-2xl text-white">Get Started Now!</p>
-          </div>
+
+          <UploadForm />
         </div>
       </main>
     </HydrateClient>
