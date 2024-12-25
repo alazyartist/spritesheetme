2212874aea18330dc2b33671bee706adeda4cd2b
Dylan James <alazyartist@gmail.com>
2024-12-24 23:56:26 -0700

    Sidebar size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default async function Home() {
       <main className="dark flex h-full max-h-[calc(100%-4rem)] w-[calc(100%-8px)] flex-col items-center justify-start text-white">
         <div className="container flex h-full flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Sprite <span className="text-[hsl(176,64%,55%)]">Sheet</span> ME
+            sprite<span className="text-sky-400">sheet</span> me
           </h1>
-
+          <p>Upload a character sprite and generate a spritesheet</p>
           <UploadForm />
         </div>
       </main>
